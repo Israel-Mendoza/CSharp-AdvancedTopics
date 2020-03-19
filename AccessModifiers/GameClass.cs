028bc6e12c11427d489a1d7f4435e95d720b2dc8
Israel Mendoza <xander_irsl@hotmail.com>
2020-03-18 23:20:09 -0600

    Initial commit on Solution containing Advanced C Sharp concepts

---
+++ AccessModifiers/GameClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Structs
+{
+    class GameClass
+    {
+        // Fields
+        public string name;
+        public string developer;
+        public double rating;
+        public string releaseDate;
+
+        // Constructor
+        public GameClass(string name, string developer, double rating, string releaseDate)
+        {
+            this.name = name;
+            this.developer = developer;
+            this.rating = rating;
+            this.releaseDate = releaseDate;
+        }
+
+        // Methods
+        public void Display()
+        {
+            Console.WriteLine($"Game name: {name}\nDeveloped by: {developer}\nRelease date: {releaseDate}\nRating: {rating}");
+        }
+    }
+}
---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Manipulating a struct
+            
+            Console.WriteLine("\nWORKING WITH A STRUCT!!!\n");
+
+            GameStruct myGameStructObj = new GameStruct("Pokemon", "Niantic", 4.1, "06.01.2016");
+
+            Console.WriteLine("Calling the Display() method on the original Game object: ");
+            myGameStructObj.Display();
+
+            Console.WriteLine("\nCalling the ChangeGameName() static method with the previously displayed game...\n");
+            ChangeGameStructObjName(myGameStructObj);
+            Console.WriteLine("\nCalling the Display() method on the recently changed struct FROM OUTSIDE THE CHANGEGAMEMETHOD()!!!\n");
+            myGameStructObj.Display();
+
+            // Same example, but with a class
+
+            Console.WriteLine("\nWORKING WITH A CLASS!!!\n");
+            
+            GameClass myGameClassObj = new GameClass("Pokemon", "Niantic", 4.1, "06.01.2016");
+
+            Console.WriteLine("Calling the Display() method on the original Game object: ");
+            myGameClassObj.Display();
+
+            Console.WriteLine("\nCalling the ChangeGameName() static method with the previously displayed game...\n");
+            ChangeGameClassObjName(myGameClassObj);
+            Console.WriteLine("\nCalling the Display() method on the recently changed struct FROM OUTSIDE THE CHANGEGAMEMETHOD()!!!\n");
+            myGameClassObj.Display();
+
+
+        }
+
+        static void ChangeGameStructObjName(GameStruct aGameObj)
+        {
+            Console.WriteLine($"\nName of the passed Game (Game.name) = {aGameObj.name}");
+            Console.WriteLine($"Changing the name in the method from {aGameObj.name} to \"Candy Crush\"...");
+            aGameObj.name = "Candy Crush";
+            Console.WriteLine("Calling the Display() method on the recently changed struct FROM WITHIN THE CHANGEGAMEMETHOD()!!!\n");
+            aGameObj.Display();
+        }
+
+        static void ChangeGameClassObjName(GameClass aGameObj)
+        {
+            Console.WriteLine($"\nName of the passed Game (Game.name) = {aGameObj.name}");
+            Console.WriteLine($"Changing the name in the method from {aGameObj.name} to \"Candy Crush\"...");
+            aGameObj.name = "Candy Crush";
+            Console.WriteLine("Calling the Display() method on the recently changed struct FROM WITHIN THE CHANGEGAMEMETHOD()!!!\n");
+            aGameObj.Display();
+        }
+    }
+}
