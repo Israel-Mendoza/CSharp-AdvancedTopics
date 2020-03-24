eeb5a1b72a200b627753547b7ba34e4d981f82a2
Israel Mendoza <xander_irsl@hotmail.com>
2020-03-23 20:48:34 -0500

    Added project with Nulable Types

---
+++ Nullables/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Nullables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? num1 = null;
+            int? num2 = 1337;
+
+            double? d1 = new double?();
+            double? d2 = 3.141592;
+
+            bool? myBool = null;
+
+            Console.WriteLine($"My nullable values are '{num1}', '{num2}', '{d1}', '{d2}', and '{myBool}'");
+
+            DisplayGender(myBool);
+
+            bool validGender = myBool ?? false;
+
+            Console.WriteLine($"A valid gender: {validGender}");
+        }
+
+        static void DisplayGender(bool? isMale)
+        {
+            if (isMale == true) Console.WriteLine("Gender chosen: Male");
+            else if (isMale == false) Console.WriteLine("Gender chosen: Female");
+            else Console.WriteLine("Gender not specified...");
+        }
+    }
+}
---
+++ WorkingWithDateTimes2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace WorkingWithDateTimes2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = new DateTime(1992, 04, 9, 9, 54, 0);
+
+            Console.WriteLine($"Esta es la fecha en la que nací: {now.ToString("D", new CultureInfo("es-MX"))}");
+
+        }
+    }
+}
